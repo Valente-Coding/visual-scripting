b17b82cc0880a12c413783ba07a43bf9d030116b
Valente-Coding <goncalo.p.valente@hotmail.com>
2023-08-16 20:20:29 +0100

    Added Check For Membership

--- GetMemberComponents.js
+++ GetMemberComponents.js
@@ -1,58 +1,40 @@
-// Replace these with your actual values
-const clientId = '1140613904708411463';
-const redirectUri = 'https://valente-coding.github.io/visual-scripting/';
-const scope = 'identify guilds';
-
-// Handle the OAuth2 callback
 const handleCallback = async () => {
-  const urlParams = new URLSearchParams(window.location.search);
-  const code = urlParams.get('code');
+  // Parse the URL to get the authorization code
+  const queryParams = new URLSearchParams(window.location.search);
+  const authorizationCode = queryParams.get('code');
+  const redirect_uri = "https://valente-coding.github.io/visual-scripting/"
 
-  if (code) {
-    const response = await fetch('https://discord.com/api/oauth2/token', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: new URLSearchParams({
-        client_id: clientId,
-        client_secret: 'CzS8SIm_x3hBYQ6ouVu1CZS4KMK3Yl6g',
-        grant_type: 'authorization_code',
-        code: code,
-        redirect_uri: redirectUri,
-        scope: scope,
-      }),
-    });
-
-    const data = await response.json();
+  // Exchange authorization code for access token
+  fetch('https://www.patreon.com/api/oauth2/token', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: `code=${authorizationCode}&client_id=1-DqtWay37mEkp3I-6gtUQ-a1O_2IIBRB8wnnrON9sphlbUoCj42Ep_1VECAv4oN&client_secret=KNNTFHkJVgWhhWjThCfgRO4Qj9zfzeehsEROz-If38_1qgh7Yz3ZYDPnMSuztJYA&grant_type=authorization_code&redirect_uri=${redirect_uri}`,
+  })
+  .then(response => response.json())
+  .then(data => {
     const accessToken = data.access_token;
 
-    // Use the access token to make API requests
-    const guildsResponse = await fetch('https://discord.com/api/v10/users/@me/guilds', {
+    // Fetch user data and check membership
+    fetch('https://www.patreon.com/api/oauth2/v2/identity', {
       headers: {
         Authorization: `Bearer ${accessToken}`,
       },
-    });
+    })
+    .then(response => response.json())
+    .then(userData => {
+      const hasMembership = userData.included.some(item => item.type === 'tier');
 
-    const guildsData = await guildsResponse.json();
-
-    // Assuming guildsData is an array of guild objects
-    for (const guild of guildsData) {
-      const rolesResponse = await fetch(`https://discord.com/api/v10/guilds/${guild.id}/members/@me`, {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      });
-
-      const rolesData = await rolesResponse.json();
-
-      // Assuming rolesData.roles is an array of role objects
-      if (rolesData.roles.some(role => role.id === '1140576961916309584')) {
-        console.log(`User has the specific role in guild: ${guild.name}`);
+      if (hasMembership) {
+        // User has a membership
+        console.log('User has a membership');
+      } else {
+        // User does not have a membership
+        console.log('User does not have a membership');
       }
-    }
-  }
+    });
+  });
 };
 
-// Call the callback handler when the page loads
 window.addEventListener('load', handleCallback);
